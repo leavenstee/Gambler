3364b9c5f499215b1b00bd3f7a8791c4ff952d11
Steven Lee <stevenleedos@gmail.com>
2019-04-11 20:26:47 -0500

    Initial Commit

---
+++ GamblerTests/GamblerTests.swift
@@ -0,0 +1,34 @@
+//
+//  GamblerTests.swift
+//  GamblerTests
+//
+//  Created by Steven Lee on 4/11/19.
+//  Copyright © 2019 leavenstee llc. All rights reserved.
+//
+
+import XCTest
+@testable import Gambler
+
+class GamblerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
